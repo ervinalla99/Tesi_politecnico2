022d24eeb6b9037d18da2f8133bf71d46e4628dd
Ervin <ervin.alla@pini.group>
2024-09-15 21:28:16 +0200

    New model

---
+++ README.md
@@ -0,0 +1,8 @@
+This Repository contains a web IFC viewer integrated with an interactive Dialogflow CX chatbot
+
+![image](https://github.com/user-attachments/assets/158e5f6b-3510-44c3-90db-907e74061bbe)
+
+![image](https://github.com/user-attachments/assets/6c921fc5-375a-4b74-b72a-5417a73d8e86)
+
+![image](https://github.com/user-attachments/assets/e44d9122-dac2-4429-9f9a-8bb03db5df84)
+
--- src/components/Panels/Help.ts
+++ src/components/Panels/Help.ts
@@ -6,7 +6,7 @@ export default BUI.Component.create<BUI.Panel>(() => {
     <bim-panel>
       <bim-panel-section fixed label="Get the full code of this app" icon="ic:baseline-people">
         <bim-label style="white-space: normal;">If you want to see the full code that was used to build this app, please visit the link in the button below.</bim-label>
-        <bim-button @click=${() => open("https://people.thatopen.com/")} label="Github Repository" icon="ic:baseline-people"></bim-button>
+        <bim-button @click=${() => open("https://github.com/ervinalla99/Tesi_Politecnico")} label="GitHub Repository" icon="ic:baseline-people"></bim-button>
         <div style="display: flex; justify-content: center; align-items: center; background-color: #f0f0f0; padding: 10px; margin-bottom: 10px;">
       <img src="../../logo.png" alt="Logo" style="width: 100px; height: auto;">
     </div>
